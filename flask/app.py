dc5b05c82a08a573e8478d185adc9da6630fcb52
Naerhy <naerhy@protonmail.com>
2022-06-08 15:31:23 +0200

    Add vaults list and request form handling (app.py)

--- flask/app.py
+++ flask/app.py
@@ -4,27 +4,36 @@ from web3 import Web3
 import json
 
 app = Flask(__name__)
+PROVIDER = "https://ropsten.infura.io/v3/3bf84785afd6464ba615ed0c903d9878"
+
+def convert_json(file):
+    with open(file) as f:
+        return json.load(f)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        # connect to ALL web3 providers
-        # check return errors
-        # declare contract address
-        # get abi
-        # declare web3 contract
-        # check nb_vaults function and exit if == 0
+        web3 = Web3(Web3.HTTPProvider(PROVIDER))
+        # if not web3.isConnected():
+            # exit program or display error page
+        va_address = "0x1eEC5Ed724aC0cADDa528550DD5136cDFd317Db7"
+        va_abi = convert_json("Vaults.json")
+        va_contract = web3.eth.contract(address=va_address, abi=va_abi)
+        nb_vaults = va_contract.functions.nbVaults().call()
+        # if nb_vaults == 0:
+            # exit program or display error page
         user_address = request.form["address"]
-        if Web3.isAddress(user_address):
-            user_address = Web3.toChecksumAddress(user_address)
-            # vaults = set()
-            # for i in range(nb_vaults):
-                # vault = xxx.functions.vaults(i).call()
-                # if (vault[2] == user_address:
-                    # vaults.add(vault)
-        return render_template("vaults.html", vaults)
+        # if not Web3.isAddress(user_address):
+            # exit program or display error page
+        user_address = Web3.toChecksumAddress(user_address)
+        vaults = []
+        for i in range(nb_vaults):
+            vault = va_contract.functions.vaults(i).call()
+            if vault[2] == user_address:
+                vaults.append(vault)
+        return render_template("index.html", vaults=vaults)
     else:
-        return render_template("index.html")
+        return render_template("index.html", vaults=None)
 
 if __name__ == "__main__":
     # disable debug mode when deployed to live servers
